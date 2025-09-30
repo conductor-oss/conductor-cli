fd145fbf75a5fa73eb19761613b7eefa2f25d79a
Miguel Prieto <miguel.prieto@orkes.io>
2025-09-30 17:55:05 -0300

    Added persistent auth config using Viper.

--- cmd/root.go
+++ cmd/root.go
@@ -2,14 +2,16 @@ package cmd
 
 import (
 	"context"
-	"io"
-	stdlog "log"
+	"fmt"
 	"github.com/conductor-sdk/conductor-go/sdk/client"
-	"github.com/conductor-sdk/conductor-go/sdk/settings"
 	sdklog "github.com/conductor-sdk/conductor-go/sdk/log"
+	"github.com/conductor-sdk/conductor-go/sdk/settings"
 	"github.com/orkes-io/conductor-cli/internal"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"io"
+	stdlog "log"
 	"net"
 	"net/http"
 	"os"
@@ -19,6 +21,7 @@ import (
 var NAME = "cdt"
 
 var (
+	cfgFile string
 	url     string
 	key     string
 	secret  string
@@ -37,18 +40,18 @@ var rootCmd = &cobra.Command{
 		if verbose {
 			log.SetLevel(log.DebugLevel)
 		}
-		serverConfig := getActiveConfig()
-		if serverConfig == nil {
-			serverConfig = &Config{
-				URL: "http://localhost:8080/api",
-			}
-		}
-		log.Debug("Using Server ", serverConfig.URL)
+		// Get configuration values from Viper (which handles flags, env vars, and config file)
+		url = viper.GetString("server")
+		key = viper.GetString("auth-key")
+		secret = viper.GetString("auth-secret")
+		token = viper.GetString("auth-token")
 
-		url = serverConfig.URL
-		key = serverConfig.Key
-		secret = serverConfig.Secret
-		token = serverConfig.Token
+		// Set default URL if not provided
+		if url == "" {
+			url = "http://localhost:8080/api"
+		}
+
+		log.Debug("Using Server ", url)
 		apiClient := client.NewAPIClient(settings.NewAuthenticationSettings(key, secret), settings.NewHttpSettings(url))
 
 		if token != "" {
@@ -97,22 +100,66 @@ func getHttpClient() *http.Client {
 
 }
 
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".conductor-cli")
+	}
+
+	// Environment variable mapping
+	viper.SetEnvPrefix("CONDUCTOR")
+	viper.AutomaticEnv()
+
+	// Map environment variables to config keys
+	viper.BindEnv("server", "CONDUCTOR_SERVER_URL")
+	viper.BindEnv("auth-key", "CONDUCTOR_AUTH_KEY")
+	viper.BindEnv("auth-secret", "CONDUCTOR_AUTH_SECRET")
+	viper.BindEnv("auth-token", "CONDUCTOR_AUTH_TOKEN")
+
+	if err := viper.ReadInConfig(); err == nil {
+		if viper.GetBool("verbose") {
+			fmt.Fprintf(os.Stderr, "Using config file: %s\n", viper.ConfigFileUsed())
+		}
+	}
+}
+
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	// Suppress debug logs from conductor-go SDK
 	stdlog.SetOutput(io.Discard)
 	stdlog.SetFlags(0)
-	
+
 	// Disable conductor-go SDK logging by using the noop logger
 	sdklog.SetLogger(sdklog.NewNop())
-	
-	rootCmd.PersistentFlags().StringVar(&url, "url", "", "server url")
-	rootCmd.PersistentFlags().StringVar(&token, "token", "", "auth token")
-	rootCmd.PersistentFlags().StringVar(&key, "key", "", "auth key")
-	rootCmd.PersistentFlags().StringVar(&secret, "secret", "", "auth secret")
 
+	// Configuration file flag
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.conductor-cli.yaml)")
+
+	// Server and authentication flags
+	rootCmd.PersistentFlags().String("server", "", "Conductor server URL (can also be set via CONDUCTOR_SERVER_URL)")
+	rootCmd.PersistentFlags().String("auth-key", "", "API key for authentication (can also be set via CONDUCTOR_AUTH_KEY)")
+	rootCmd.PersistentFlags().String("auth-secret", "", "API secret for authentication (can also be set via CONDUCTOR_AUTH_SECRET)")
+	rootCmd.PersistentFlags().String("auth-token", "", "Auth token for authentication (can also be set via CONDUCTOR_AUTH_TOKEN)")
+
+	// Other flags
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "print verbose logs")
-	rootCmd.MarkFlagsMutuallyExclusive("key", "token")
-	rootCmd.MarkFlagsMutuallyExclusive("secret", "token")
 	rootCmd.PersistentFlags().BoolVarP(&yes, "yes", "y", false, "confirm yes")
 
+	// Bind flags to viper
+	viper.BindPFlag("server", rootCmd.PersistentFlags().Lookup("server"))
+	viper.BindPFlag("auth-key", rootCmd.PersistentFlags().Lookup("auth-key"))
+	viper.BindPFlag("auth-secret", rootCmd.PersistentFlags().Lookup("auth-secret"))
+	viper.BindPFlag("auth-token", rootCmd.PersistentFlags().Lookup("auth-token"))
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+
+	// Mark mutually exclusive flags
+	rootCmd.MarkFlagsMutuallyExclusive("auth-key", "auth-token")
+	rootCmd.MarkFlagsMutuallyExclusive("auth-secret", "auth-token")
 }
